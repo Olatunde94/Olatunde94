62ea5c9095e364819fba230b14d965ade295f85c
Olatunde94 <61957231+Olatunde94@users.noreply.github.com>
2020-05-07 10:21:06 +0100

    Add files via upload
    Game app

---
+++ question.js
@@ -0,0 +1,37 @@
+var questions= [{
+    "question":"Scarcity implies that",
+        "option1":"Goods cannot be produced efficiently",
+       "option2":"Human wants are unlimited",
+       "option3":"Goods and services are limited in supply",
+       "option4":"Commodities are evenly distributed within the economy",
+       "answer":"3"
+    },{
+        "question":"Which of the following is a measure of central tendency",
+        "option1":"Percentage",
+       "option2":"Graph",
+       "option3":"Variance",
+       "option4":"Median",
+       "answer":"4"
+    },{
+            "question":"One important feature of sole proprietorship is that",
+            "option1":"It is a limited liability business",
+           "option2":"The business is owned by shareholders",
+           "option3":"One person bears all the risk",
+           "option4":"It is a legal entity",
+           "answer":"3"
+    },{
+        "question":"Which of the following is an item of government recurrent expenditure",
+        "option1":"Road construction",
+       "option2":"Port facilities",
+       "option3":"Wages and salaries",
+       "option4":"Telecommunication projects",
+       "answer":"3"
+    },{
+        "question":"A collection of data which has not been processed is referred to as",
+        "option1":"Disjointed data",
+       "option2":"Raw data",
+       "option3":"Class intervals",
+       "option4":"Frequency",
+       "answer":"2"
+    }
+   ]
